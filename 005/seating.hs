3e303bce935eaeed753c4cee36bba13a493a466f
auburn <auburnete@gmail.com>
2020-12-05 15:43:46 +1000

    haskell, day 5. I missed the obvious "replace F with 0 and B with 1" solution, so ended up doing a recursive splitting thing instead ugh
    also learnt that haskell's max function is maximum and not max
    
    I've been trying to incorporate the function application operator ($) into my
    stuff a little more? I feel like it makes it harder to read tbh but whatever

---
+++ 005/seating.hs
@@ -0,0 +1,41 @@
+module Seating where
+
+data Divider
+    = First
+    | Last
+    deriving (Show)
+
+-- Parse a string into a list of dividers.
+parseChar :: Char -> Divider
+parseChar 'F' = First
+parseChar 'B' = Last
+parseChar 'L' = First
+parseChar 'R' = Last
+
+parseString = map parseChar
+
+-- get a number
+getNum :: Int -> Int -> [Divider] -> Int
+getNum _ accum [] = accum
+getNum i accum (First:xs) = getNum (i `div` 2) accum xs
+getNum i accum (Last:xs) = getNum (i `div` 2) (accum + i `div` 2) xs
+
+seatId :: String -> Int
+seatId s
+    = getNum 128 0 a * 8 + getNum 8 0 b
+    where
+        a = parseString $ take 7 s
+        b = parseString $ drop 7 s
+
+allSeatIds = map seatId . lines
+
+maxSeatId :: String -> Int
+maxSeatId s = maximum (allSeatIds s)
+
+mySeat :: [Int] -> Int
+mySeat i = head [a - 1 | a <- i, b <- i, a - b == 2 && notElem (a - 1) i]
+
+main :: IO()
+main = do
+    content <- readFile "input"
+    (print . mySeat . allSeatIds) content
