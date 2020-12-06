f35a34f1785ad4f0ce185f6a3a2b0210c9318422
auburn <auburnete@gmail.com>
2020-12-06 15:56:52 +1000

    haskell, day 6! I learnt about `foldl` and `foldl1` which is super useful

---
+++ 006/questions2.hs
@@ -0,0 +1,29 @@
+module Questions2 where
+
+import qualified Data.Text as T
+import Data.List
+import Data.Char (isAlpha)
+
+groups :: T.Text -> [T.Text]
+groups = T.splitOn (T.pack "\n\n")
+
+uniqueChars = T.foldl f []
+    where
+        f :: [Char] -> Char -> [Char]
+        f arr c
+            | notElem c arr && isAlpha c = c : arr
+            | otherwise     = arr
+
+processGroup :: T.Text -> Int
+processGroup t
+    = length (foldl1 intersect qs)
+    where
+        qs = map uniqueChars (T.lines t)
+
+process :: T.Text -> Int
+process t = sum (map processGroup (groups t))
+
+main :: IO()
+main = do
+    content <- readFile "input"
+    (print . process . T.pack) content
